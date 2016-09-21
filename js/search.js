fa8b7c828131be86a7c5570fe89f14f051f22b18
Drew Keller <akeller@mitre.org>
2016-09-21 09:13:57 -0400

    #4: removes h3 tag from results

--- js/search.js
+++ js/search.js
@@ -7,7 +7,7 @@
 
       for (var i = 0; i < results.length; i++) {  // Iterate over the results
         var item = store[results[i].ref];
-        appendString += '<li><a href="' + item.url + '"><h3>' + item.title + '</h3></a>';
+        appendString += '<li><a href="' + item.url + '">' + item.title + '</a></li>';
       }
 
       searchResults.innerHTML = appendString;
