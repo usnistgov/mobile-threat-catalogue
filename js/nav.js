0be119d5e6efb20a238de770fb40f8a930034e19
jajmo <jason.ajmo@nist.gov>
2019-08-14 09:37:37 -0400

    Add rotating caret next to categories dropdown

--- js/nav.js
+++ js/nav.js
@@ -1,5 +1,8 @@
 function toggleNav()
 {
     let element = document.getElementById('nav-links');
+    let caret = document.getElementsByClassName('down-caret')[0];
+
     element.style.maxHeight = (element.style.maxHeight) ? null : element.scrollHeight + "px";
+    caret.classList.toggle("open-caret");
 }
